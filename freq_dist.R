76b7a07f9e97e6d9ae8a6f3be265608a0bf549d1
saur1n <dr.saurin.parikh@gmail.com>
2018-10-11 12:12:22 -0400

    initial commit

--- freq_dist.R
+++ freq_dist.R
@@ -2,3 +2,13 @@
 ##### proto-gene frequency distribution plots
 ##### Author  : Saurin Parikh (dr.saurin.parikh@gmail.com)
 ##### Date    : 10/11/2018
+
+library(RMariaDB)
+source("R/functions/initialize.sql.R")
+
+mydb <- initialize.sql("saurin_test")
+
+dbListFields(mydb, '1536_PS2_previous')
+
+
+dbDisconnect(mydb)
