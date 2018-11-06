b6d271d209765e6d641717ee19f0d5218e4b45e8
saur1n <dr.saurin.parikh@gmail.com>
2018-11-06 11:50:09 -0500

    upudate for ybr data

--- scripts/freq_dist.R
+++ scripts/freq_dist.R
@@ -13,8 +13,8 @@ conn <- initialize.sql("saurin_test")
 
 ##### FETCH DATA
 
-query = dbSendQuery(conn, ("select orf_name, fitness from PT_SA_CN_6144_FITNESS
-where hours = 12 and orf_name = 'BF_control' and fitness > 0"))
+query = dbSendQuery(conn, ("select orf_name, fitness from YBR_RAF_6144_FITNESS
+where hours = 30 and orf_name = 'BF_control' and fitness > 0"))
 data.control = dbFetch(query, n=-1)
 
 if (dbHasCompleted(query)) {
@@ -23,12 +23,8 @@ if (dbHasCompleted(query)) {
   print("Error Running Query")
 }
 
-query = dbSendQuery(conn, ("select orf_name, fitness from PT_SA_CN_6144_FITNESS
-where hours = 12 and fitness > 1.07
-and orf_name in
-(select orf_name from PT_SA_CN_6144_RES_eFDR
-where hours = 12 and effect_cs = 1)
-order by fitness desc"))
+query = dbSendQuery(conn, ("select orf_name, fitness from YBR_RAF_6144_FITNESS
+where hours = 30 and orf_name = 'YBR196C-A' and fitness > 0"))
 data.orfs = dbFetch(query, n=-1)
 
 if (dbHasCompleted(query)) {
@@ -37,19 +33,19 @@ if (dbHasCompleted(query)) {
   print("Error Running Query")
 }
 
-query = dbSendQuery(conn, ("select orf_name, fitness from PT_SA_CN_6144_FITNESS
-where hours = 12 and fitness between 0 and 0.90
-and orf_name in
-(select orf_name from PT_SA_CN_6144_RES_eFDR
-where hours = 12 and effect_cs = -1)
-order by fitness desc"))
-data.dels = dbFetch(query, n=-1)
+#query = dbSendQuery(conn, ("select orf_name, fitness from PT_SA_CN_6144_FITNESS
+#where hours = 12 and fitness between 0 and 0.90
+#and orf_name in
+#(select orf_name from PT_SA_CN_6144_RES_eFDR
+#where hours = 12 and effect_cs = -1)
+#order by fitness desc"))
+#data.dels = dbFetch(query, n=-1)
 
-if (dbHasCompleted(query)) {
-  dbClearResult(query)
-} else {
-  print("Error Running Query")
-}
+#if (dbHasCompleted(query)) {
+#  dbClearResult(query)
+#} else {
+#  print("Error Running Query")
+#}
 
 #data.dels$fitness[data.dels$fitness < 0]
 
@@ -59,9 +55,27 @@ if (dbHasCompleted(query)) {
 #  geom_density(alpha = 0.3) +
 #  geom_density(data = data.control, alpha = 0.3)
 
+#ggplot() +
+#  geom_density(data = data.dels, aes(x=fitness) ,fill="#D32F2F", color="#757575",alpha = 0.5) +
+#  #scale_y_continuous(trans="log10") +
+#  geom_density(data = data.orfs, aes(x=fitness), fill="#4CAF50", color="#757575", alpha = 0.5) +
+#  geom_density(data = data.control, aes(x=fitness), fill="#303F9F", color="#757575", alpha = 0.5) +
+#  scale_x_continuous(breaks = round(seq(0, 2, by = 0.1),1)) +
+#  labs(title = "Probability Distribution", x = "Fitness", y = "Density", fill = "Strain Type", col = "Strain Type") +
+#  #scale_fill_manual(labels = c("Deleterious", "Beneficial", "Reference"), values = c("#F44336", "#3F51B5", "#BDBDBD")) +
+#  theme_gray()+
+#  theme(axis.text=element_text(size=14),
+#        axis.title=element_text(size=20),
+#        plot.title=element_text(size=20,hjust =.5),
+#        #panel.grid.major = element_blank(),
+#        #panel.grid.minor = element_blank(),
+#        panel.background = element_rect(fill = "transparent", colour = NA),
+#        plot.background = element_rect(fill = "transparent", colour = NA))
+#ggsave("figs/overall_distribution_PT_SA_noback.png",bg="transparent",height = 9, width = 12)
+
+##### YBR DATA
+
 ggplot() +
-  geom_density(data = data.dels, aes(x=fitness) ,fill="#D32F2F", color="#757575",alpha = 0.5) +
-  #scale_y_continuous(trans="log10") +
   geom_density(data = data.orfs, aes(x=fitness), fill="#4CAF50", color="#757575", alpha = 0.5) +
   geom_density(data = data.control, aes(x=fitness), fill="#303F9F", color="#757575", alpha = 0.5) +
   scale_x_continuous(breaks = round(seq(0, 2, by = 0.1),1)) +
@@ -75,8 +89,8 @@ ggplot() +
         #panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "transparent", colour = NA),
         plot.background = element_rect(fill = "transparent", colour = NA))
-ggsave("figs/overall_distribution_PT_SA_noback.png",bg="transparent",height = 9, width = 12)
-  
+#ggsave("figs/overall_distribution_PT_SA_noback.png",bg="transparent",height = 9, width = 12)
+
 ##### END
 dbDisconnect(conn)
 
