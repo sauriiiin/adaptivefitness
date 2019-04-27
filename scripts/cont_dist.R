1058e50ae4f652cee34d1d6f8440581ac51f16a7
saur1n <dr.saurin.parikh@gmail.com>
2019-04-26 22:35:08 -0400

    update

--- scripts/cont_dist.R
+++ scripts/cont_dist.R
@@ -44,3 +44,35 @@ query = dbSendQuery(conn, sprintf('select distinct %s from %s a order by %s asc'
                                   p2c_info[1],
                                   p2c_info[2]))
 n_plates = dbFetch(query, n=-1) 
+
+#####
+for (hr in 7:length(hours$hours)) {
+  for (pl in n_plates$`6144plate`) {
+    query = dbSendQuery(conn, sprintf('select * from %s a, %s b where a.hours = %d and a.pos = b.pos and b.%s = %d order by b.%s, b.%s',
+                                      tablename_fit,p2c_info[1],hours$hours[hr],p2c_info[2],
+                                      pl,p2c_info[3],p2c_info[4]))
+    fitdat = dbFetch(query,n=-1)
+    fitdat$fitness[fitdat$orf_name != 'BF_control'] = NA
+    
+    query = dbSendQuery(conn, sprintf("select a.* from %s a, %s b where a.hours = %d and a.pos = b.pos and b.%s = %d and orf_name = '%s' order by b.%s, b.%s",
+                                      tablename_fit,p2c_info[1],hours$hours[hr],p2c_info[2],
+                                      pl,cont.name,p2c_info[3],p2c_info[4]))
+    contpos = dbFetch(query,n=-1)
+    
+  }
+}
+
+hello = fitdat
+hello$fitness[hello$orf_name != 'BF_control'] = NA
+lims = quantile(hello$fitness, c(.025, .975),na.rm = TRUE) 
+
+hello$hello[hello]
+
+ggplot(data = hello, aes(x = `6144col`, y = `6144row`, size = fitness, col = fitness)) +
+  geom_point()
+
+
+
+
+
+
