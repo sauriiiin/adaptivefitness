6c44638b4baaa4c0b51dad01e39ab6576a17dc01
saur1n <dr.saurin.parikh@gmail.com>
2019-05-08 17:18:07 -0400

    updated with 96plate layout

--- scripts/exp2_qc.R
+++ scripts/exp2_qc.R
@@ -81,7 +81,74 @@ for (pl in 23:28) {
 }
 
 
+##### GET/SET 96 DATA
+bc <- dbGetQuery(conn,
+                 'select ((b.96plate-1)*96 + ((b.96col-1)*8) + b.96row) as pos
+                 from BACTERIAL_SPACE_MAP b
+                 where strain_id > 0')
+bc$pos <- as.integer(bc$pos)
 
+pc <- dbGetQuery(conn,
+                  'select ((b.96plate-1)*96 + ((b.96col-1)*8) + b.96row) as pos
+                  from PROTOGENE_COLLECTION a, BACTERIAL_SPACE_MAP b
+                  where a.strain_id = b.strain_id')
+pc$pos <- as.integer(pc$pos)
+
+pos2coor96 <- dbGetQuery(conn,
+                          'select * from PT_pos2coor96
+                          order by 96col, 96row')
+pos2coorPC <- NULL
+for (i in 1:29) {
+  pos2coor96$`96plate` <- i
+  pos2coorPC <- rbind(pos2coorPC,pos2coor96)
+}
+pos2coorPC$pos <- (pos2coorPC$`96plate`-1)*96 + ((pos2coorPC$`96col`-1)*8) + pos2coorPC$`96row`
+
+qc.res <- dbGetQuery(conn,
+                      'select ((b.96plate-1)*96 + ((b.96col-1)*8) + b.96row) as pos,
+                      a.score, a.qc, a.flag, a.comments
+                      from QC_SEQ_ANALYSIS a, BACTERIAL_SPACE_MAP b
+                      where a.strain_id = b.strain_id
+                      order by b.96plate, b.96col, b.96row')
+qc.res$pos <- as.integer(qc.res$pos)
+
+pos2coorPC$seq[pos2coorPC$pos %in% qc.res$pos] = 1
+pos2coorPC$flag[pos2coorPC$pos %in% qc.res$pos[qc.res$flag == 2]] = 2
+pos2coorPC$mismatch[pos2coorPC$pos %in% qc.res$pos[qc.res$comments == 'switched to other orf']] = 1
+pos2coorPC$result[pos2coorPC$pos %in% bc$pos] = 'ORF Present'
+pos2coorPC$result[pos2coorPC$pos %in% pc$pos] = 'EXP'
+pos2coorPC$result[pos2coorPC$pos %in% qc.res$pos] = 'Sequenced'
+pos2coorPC$result[pos2coorPC$pos %in% qc.res$pos[qc.res$flag == 2]] = 'Flagged'
+pos2coorPC$result[pos2coorPC$pos %in% qc.res$pos[qc.res$comments == 'switched to other orf']] = 'Mismatch'
+
+##### VISUALIZE DATA
+
+for (pl in 1:29) {
+  ggplot(data = pos2coorPC[pos2coorPC$`96plate` == pl,], aes(x = `96col`, y = `96row`)) +
+    geom_point(aes(x = `96col`, y = `96row`),shape = 20,size=1,na.rm = T) +
+    geom_point(aes(x = `96col`, y = `96row`,col = result, shape = result),size=5,na.rm = T) +
+    #geom_point(aes(size = average, col = prediction, shape = colsize),alpha=0.9) +
+    #scale_size_continuous(name="Results") +
+    labs(title = "Entry clone collection",
+         subtitle = sprintf("Plate %d",pl),
+         x = " Column",
+         y = "Row") +
+    scale_x_continuous(breaks = seq(1,12,1), minor_breaks = seq(-2,14,1)) +
+    scale_y_continuous(breaks = seq(1,8,1), minor_breaks = seq(-2,10,1), trans = 'reverse') +
+    scale_colour_manual(name="Result",
+                        values=c("Flagged"="#D32F2F","ORF Present"="#9E9E9E",
+                                 "EXP"="#9C27B0","Sequenced"="#388E3C","Mismatch" = "#536DFE"),
+                        breaks=c("ORF Present","EXP","Sequenced","Mismatch","Flagged")) +
+    scale_shape_manual(name="Colony Kind",
+                       values=c("ORF Present"=19,"EXP"=19,"Sequenced"=19,"Mismatch"=19,"Flagged"=8),
+                       breaks=c("ORF Present","EXP","Sequenced","Mismatch","Flagged"),
+                       guide=F) +
+    theme_linedraw() +
+    theme(axis.text.x = element_text(size=7),
+          axis.text.y = element_text(size=7))
+  ggsave(sprintf("figs/pc_plates/ec_plates/ECPlate%d.png",pl),
+         width = 15,height = 10) 
+}
 
 
 
