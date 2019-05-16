0df1da30aa9bf8f2b1a2c268e714741374c02825
saur1n <dr.saurin.parikh@gmail.com>
2019-05-16 16:43:35 -0400

    update

--- scripts/4C_perc.R
+++ scripts/4C_perc.R
@@ -114,7 +114,7 @@ n_plates = dbGetQuery(conn, sprintf('select distinct %s from %s a order by %s as
                                     p2c_info[1],
                                     p2c_info[2]))
 
-for (hr in hours[[1]]) {
+for (hr in hours[[1]][7:length(hours[[1]])]) {
   for (pl in n_plates[[1]]) {
     fitdat = dbGetQuery(conn, sprintf('select a.*, b.*
                                       from %s a, %s b
@@ -130,46 +130,106 @@ for (hr in hours[[1]]) {
     fitdat$source[fitdat$`6144row`%%2==1 & fitdat$`6144col`%%2==0] = 'TR'
     fitdat$source[fitdat$`6144row`%%2==0 & fitdat$`6144col`%%2==0] = 'BR'
     
+    mn_avg <- NULL
+    dif_avg <- NULL
     var_avg <- NULL
     source <- NULL
-    for (col in unique(fitdat$`6144col`)[7:90]) {
-      for (row in unique(fitdat$`6144row`)[7:58]) {
+    for (col in unique(fitdat$`6144col`)[11:86]) {
+      for (row in unique(fitdat$`6144row`)[11:54]) {
         if (!is.na(fitdat$orf_name[fitdat$`6144col` == col & fitdat$`6144row` == row])) {
-          a <- fitdat$average[fitdat$`6144col` == col & fitdat$`6144row` == row]
-          u <- fitdat$average[fitdat$`6144col` == col & fitdat$`6144row` == row - 2]
-          d <- fitdat$average[fitdat$`6144col` == col & fitdat$`6144row` == row + 2]
-          l <- fitdat$average[fitdat$`6144col` == col - 2 & fitdat$`6144row` == row]
-          r <- fitdat$average[fitdat$`6144col` == col + 2 & fitdat$`6144row` == row]
-          fitdat$var[fitdat$`6144col` == col & fitdat$`6144row` == row] = sd(c(a,u,d,l,r),na.rm = T)/mean(c(a,u,d,l,r),na.rm = T)
-          var_avg <- c(var_avg, sd(c(a,u,d,l,r),na.rm = T)/mean(c(a,u,d,l,r),na.rm = T))
-          source <- c(source, fitdat$source[fitdat$`6144col` == col & fitdat$`6144row` == row])
+          if (fitdat$orf_name[fitdat$`6144col` == col & fitdat$`6144row` == row] == 'BF_control') {
+            a <- fitdat$average[fitdat$`6144col` == col & fitdat$`6144row` == row]
+            u <- fitdat$average[fitdat$`6144col` == col & fitdat$`6144row` == row - 4]
+            d <- fitdat$average[fitdat$`6144col` == col & fitdat$`6144row` == row + 4]
+            l <- fitdat$average[fitdat$`6144col` == col - 4 & fitdat$`6144row` == row]
+            r <- fitdat$average[fitdat$`6144col` == col + 4 & fitdat$`6144row` == row]
+            fitdat$var[fitdat$`6144col` == col & fitdat$`6144row` == row] = sd(c(a,u,d,l,r),na.rm = T)/mean(c(a,u,d,l,r),na.rm = T)
+            mn_avg <- c(mn_avg, mean(c(u,d,l,r),na.rm = T))
+            dif_avg <- c(dif_avg, (a - mean(c(u,d,l,r),na.rm = T))/a*100)
+            var_avg <- c(var_avg, sd(c(a,u,d,l,r),na.rm = T)/mean(c(a,u,d,l,r),na.rm = T))
+            source <- c(source, fitdat$source[fitdat$`6144col` == col & fitdat$`6144row` == row])
+          }
         }
       }
     }
-    var_avg <- data.frame(var_avg, source)
-    colnames(var_avg) <- c("var", "source")
-    ggplot(var_avg, aes(x=var, col=source)) +
+    var_avg <- data.frame(mn_avg,dif_avg,var_avg,source)
+    colnames(var_avg) <- c("mean","diff","var","source")
+    
+    # var_plot <- ggplot(var_avg, aes(x=var, col=source)) +
+    #   geom_density(lwd = 2) +
+    #   labs(title = "Variability b/w Neighbors: References",
+    #        subtitle = sprintf("%s | %d hours | Plate %d", expt, hr, pl),
+    #        x = "Neighbor Variance",
+    #        y = "Density") +
+    #   scale_colour_manual(name="Source",
+    #                       values=c("TL"="#D32F2F","TR"="#536DFE","BL"="#388E3C","BR"="#795548"),
+    #                       breaks=c("TL","TR","BL","BR"),
+    #                       labels=c("Top Left","Top Right","Bottom Left","Bottom Right")) +
+    #   scale_x_continuous(breaks = seq(0,0.5,0.05),
+    #                      minor_breaks = seq(0,0.5,0.025)) +
+    #   scale_y_continuous(breaks = seq(0,50,10),
+    #                      minor_breaks = seq(0,50,5)) +
+    #   theme_linedraw() +
+    #   theme(legend.position = c(0.9,0.83),
+    #         legend.background = element_rect(fill="lightblue", 
+    #                                          size=0.5, linetype="solid"))+
+    #   coord_cartesian(xlim = c(0, 0.15),
+    #                   ylim = c(0, 35))
+    # 
+    # avg_plot <- ggplot(fitdat[fitdat$orf_name == 'BF_control',], aes(x=average, col=source)) +
+    #   geom_density(lwd = 2) +
+    #   labs(title = "Pixel Count Distribution",
+    #        subtitle = sprintf("%s | %d hours | Plate %d", expt, hr, pl),
+    #        x = "Pixel Count",
+    #        y = "Density") +
+    #   scale_colour_manual(name="Source",
+    #                       values=c("TL"="#D32F2F","TR"="#536DFE","BL"="#388E3C","BR"="#795548"),
+    #                       breaks=c("TL","TR","BL","BR"),
+    #                       labels=c("Top Left","Top Right","Bottom Left","Bottom Right"), guide = F) +
+    #   scale_x_continuous(breaks = seq(0,1000,100),
+    #                      minor_breaks = seq(0,1000,50)) +
+    #   scale_y_continuous(breaks = seq(0,1,0.005),
+    #                      minor_breaks = seq(0,1,0.0025)) +
+    #   theme_linedraw() +
+    #   coord_cartesian(xlim = c(200,600),
+    #                   ylim = c(0, 0.02))
+    # png(sprintf("%s%s_NEIGH_AVG_%d_%d.png",
+    #             out_path,expt_name,
+    #             hr,pl),
+    #     width = 1600, height = 700)
+    # grid.arrange(avg_plot,var_plot,nrow=1)
+    # dev.off()
+    
+    ggplot(var_avg, aes(x=diff, col=source)) +
       geom_density(lwd = 2) +
-      labs(title = "Variability b/w Neighbors",
-           subtitle = sprintf("%s | %d hours | Plate %d", expt, hr, pl),
-           x = "Neighbor Variance",
+      labs(title = "Neighbor Differences: References",
+           subtitle = sprintf("%s | %d hours | Plate %d | L = %d | R = %d",
+                              expt, hr, pl,
+                              dim(var_avg[var_avg$diff < 0,])[1],
+                              dim(var_avg[var_avg$diff > 0,])[1]),
+           x = "Pixel Difference %",
            y = "Density") +
       scale_colour_manual(name="Source",
                           values=c("TL"="#D32F2F","TR"="#536DFE","BL"="#388E3C","BR"="#795548"),
                           breaks=c("TL","TR","BL","BR"),
                           labels=c("Top Left","Top Right","Bottom Left","Bottom Right")) +
-      scale_x_continuous(breaks = seq(0,0.5,0.05),
-                         minor_breaks = seq(0,0.5,0.025)) +
-      scale_y_continuous(breaks = seq(0,50,10),
-                         minor_breaks = seq(0,50,5)) +
+      scale_x_continuous(breaks = seq(-40,40,4),
+                         minor_breaks = seq(-40,40,2)) +
+      scale_y_continuous(breaks = seq(0,1,0.02),
+                         minor_breaks = seq(0,1,0.01)) +
       theme_linedraw() +
-      coord_cartesian(xlim = c(0, 0.20))
-    ggsave(sprintf("%s%s_VAR_NEIGH_AVG_%d_%d.png",
-                   out_path,expt_name,
-                   hr,pl),
-           width = 11,height = 10)
+      theme(legend.position = c(0.9,0.83),
+            legend.background = element_rect(fill="lightblue", 
+                                             size=0.5, linetype="solid")) +
+      coord_cartesian(xlim = c(-12, 16),
+                      ylim = c(0, 0.15))
+    ggsave(sprintf("%s%s_NEIGH_DIFF_%d_%d.png",
+                   out_path,expt_name,hr,pl),
+           width = 10,height = 10)
   }
 }
 
+dim(var_avg[var_avg$diff > 0,])[1]
+dim(var_avg[var_avg$diff < 0,])[1]
 
 
