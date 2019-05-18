0385be5d3bf5d06b247a19eb4602da5df09c2141
saur1n <dr.saurin.parikh@gmail.com>
2019-05-18 16:55:18 -0400

    update

--- scripts/4C_ROC.R
+++ scripts/4C_ROC.R
@@ -49,15 +49,28 @@ for (es in c(0.05,0.1,0.15,0.2,0.5)) {
   alldat <- rbind(alldat,plotdat)
 }
 
+lw = 1.2
+
 ggplot() +
-  geom_line(data = alldat[alldat$ES==50,], aes(x = FalsePositive, y = p*100, col = 'p'), linetype = "dotdash") +
-  geom_line(data = alldat[alldat$ES==5,], aes(x = FalsePositive, y = TruePositive, col = '5%')) +
-  geom_line(data = alldat[alldat$ES==10,], aes(x = FalsePositive, y = TruePositive, col = '10%')) +
-  geom_line(data = alldat[alldat$ES==15,], aes(x = FalsePositive, y = TruePositive, col = '15%')) +
-  geom_line(data = alldat[alldat$ES==20,], aes(x = FalsePositive, y = TruePositive, col = '20%')) +
-  geom_line(data = alldat[alldat$ES==50,], aes(x = FalsePositive, y = TruePositive, col = '50%')) +
-  labs(title = "LI Detector",
-       subtitle = "ROC Curve",
+  geom_line(data = alldat[alldat$ES==50,],
+            aes(x = FalsePositive, y = p*100, col = 'p'),
+            linetype = "dotdash", lwd = lw) +
+  geom_line(data = alldat[alldat$ES==5,],
+            aes(x = FalsePositive, y = TruePositive, col = '5%'),
+            lwd = lw) +
+  geom_line(data = alldat[alldat$ES==10,],
+            aes(x = FalsePositive, y = TruePositive, col = '10%'),
+            lwd = lw) +
+  geom_line(data = alldat[alldat$ES==15,],
+            aes(x = FalsePositive, y = TruePositive, col = '15%'),
+            lwd = lw) +
+  geom_line(data = alldat[alldat$ES==20,],
+            aes(x = FalsePositive, y = TruePositive, col = '20%'),
+            lwd = lw) +
+  geom_line(data = alldat[alldat$ES==50,],
+            aes(x = FalsePositive, y = TruePositive, col = '50%'),
+            lwd = lw) +
+  labs(title = "D. ROC Curve",
        x = "False Positive Rate",
        y = "Power") +
   scale_x_continuous(breaks = seq(0,100,10),
@@ -71,23 +84,23 @@ ggplot() +
                                          name='p-value')) +
   scale_colour_manual(name="Effect Size Threshold",
                       breaks=c("5%","10%","15%","20%","50%"),
-                      values=c("5%"="#D32F2F","10%"="#536DFE","15%"="#388E3C","20%"="#795548","50%"="#00BCD4","p"="#9C27B0")) +
-  theme_light() +
-  theme(axis.text.x = element_text(size=10),
-        axis.title.x = element_text(size=15),
-        axis.text.y = element_text(size=10),
-        axis.title.y = element_text(size=15),
-        axis.line.y.right = element_line(color = "#9C27B0"),
-        axis.text.y.right = element_text(size=10, color = "#9C27B0"),
-        axis.title.y.right = element_text(size=15, color = "#9C27B0"),
-        legend.text = element_text(size=13),
-        legend.title = element_text(size=15),
+                      values=c("5%"="#D32F2F","10%"="#536DFE","15%"="#388E3C",
+                               "20%"="#795548","50%"="#00BCD4","p"="#FFA000")) +
+  theme_linedraw() +
+  theme(axis.text.x = element_text(size=15),
+        axis.title.x = element_text(size=20),
+        axis.text.y = element_text(size=15),
+        axis.title.y = element_text(size=20),
+        axis.line.y.right = element_line(color = "#FFA000"),
+        axis.text.y.right = element_text(size=15, color = "#FFA000"),
+        axis.title.y.right = element_text(size=20, color = "#FFA000"),
+        legend.text = element_text(size=15),
+        legend.title = element_text(size=20),
         legend.position = "bottom",
-        plot.title = element_text(size=20,hjust = 0.5),
-        plot.subtitle = element_text(size=13,hjust = 0.5))
+        plot.title = element_text(size=25,hjust = -0.115))
 ggsave(sprintf("%s%s_ROC_%d.png",
                out_path,expt_name,hr),
-       width = 10,height = 11)
+       width = 10,height = 10.5)
 
 
 
