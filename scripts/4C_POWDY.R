286ccb2e6a00d8013e7300a0c486b1001b896c56
saur1n <dr.saurin.parikh@gmail.com>
2019-05-23 21:55:38 -0400

    update with power dynamics

--- scripts/4C_POWDY.R
+++ scripts/4C_POWDY.R
@@ -3,3 +3,78 @@
 ##### Date    : 05/23/2019
 
 ##### INITIALIZE
+library(ggplot2)
+out_path = 'figs/lid_paper/';
+dat.dir <- "/home/sbp29/R/Projects/proto_plots/rawdata/4C3_GA1_CS/"
+
+##### GET DATA
+stats.files <- list.files(path = dat.dir,
+                         pattern = "P.csv", recursive = TRUE)
+fit.files <- list.files(path = dat.dir,
+                        pattern = "S.csv", recursive = TRUE)
+hours <- as.numeric(substr(stats.files,10,11))
+dat.all <- NULL
+
+##### PUTTING IT TOGETHER
+for (i in length(hours)) {
+  hr <- hours[i]
+  dat.stats <- read.csv(paste0(dat.dir,stats.files[i]),na.strings = "NaN")
+  dat.stats$cont_hrs <- hours[i]
+  dat.fit <- read.csv(paste0(dat.dir,fit.files[i]),na.strings = "NaN")
+  cont.mean <- mean(dat.fit$fitness[dat.fit$hours == hr & dat.fit$orf_name == 'BF_control' & !is.na(dat.fit$fitness)])
+  dat.stats$es <- dat.stats$cs_mean/cont.mean
+  dat.stats$pthresh <- quantile(sort(dat.stats$p[dat.stats$hours == hr]),.05)
+  dat.all = rbind(dat.all,dat.stats)
+}
+
+# save(dat.all, file = "/home/sbp29/R/Projects/proto_plots/rawdata/4C3_GA1_CS/4C3_GA1_CS_DAT_ALL.RData")
+effect_size <- sort(unique(round(dat.all$es,3)))
+dat.pow <- NULL
+
+##### POWER CALCULATIONS
+for (es in effect_size) {
+  N <- sum(dat.all$es > es-5e-03 & dat.all$es < es+5e-03)
+  if (N > 0) {
+    TP <- sum(dat.all$es > es-5e-03 & dat.all$es < es+5e-03 & dat.all$p <= dat.all$pthresh)
+    POW <- TP/N * 100
+    dat.pow <- rbind(dat.pow, c(es,POW))
+    }
+}
+
+dat.pow <- data.frame(dat.pow)
+colnames(dat.pow) <- c("es","power")
+
+pd.cs <- ggplot(dat.pow) +
+  geom_line(aes(x = es, y = power),col = '#D32F2F', linetype = 'twodash', lwd = 2) +
+  scale_x_continuous(breaks = seq(-2,2,0.02),
+                     minor_breaks = seq(-2,2,0.01)) +
+  scale_y_continuous(breaks = seq(0,100,10),
+                     minor_breaks = seq(0,100,5)) +
+  labs(title = "Power Dynamics",
+       subtitle = "Using Cubic Spline",
+       x = "Effect Size",
+       y = "Power") +
+  theme_linedraw() +
+  theme(axis.text.x = element_text(size=15),
+        axis.title.x = element_text(size=20),
+        axis.text.y = element_text(size=15),
+        axis.title.y = element_text(size=20),
+        legend.position = c(0.8,0.2),
+        legend.background = element_blank(),
+        legend.text = element_text(size=15),
+        legend.title =  element_text(size=15),
+        plot.title = element_text(size=25,hjust = 0),
+        plot.subtitle = element_text(size=20,hjust = 0)) +
+  coord_cartesian(xlim = c(0.9,1.1),
+                  ylim = c(0,100))
+ggsave(sprintf("%spower_cs.png",out_path),
+       pd.cs,
+       width = 10,height = 10)
+
+
+
+
+
+
+
+
