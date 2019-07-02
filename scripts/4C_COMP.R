ae678f25f8a9f02b6a50eb6f502d147f318d3022
saur1n <dr.saurin.parikh@gmail.com>
2019-07-02 13:51:05 -0400

    finding the big and small colonies

--- scripts/4C_COMP.R
+++ scripts/4C_COMP.R
@@ -56,13 +56,16 @@ for (hr in hours$hours) {
     alldat$colony[alldat$orf_name != 'BF_control'] = 'Query'
     alldat$colony[is.na(alldat$orf_name)] = 'Gap'
     
+    alldat$average[alldat$colony == 'Gap'] = 0
+    
+    alldat$outlier <- NULL
     for (sr in unique(alldat$source)) {
       temp <- alldat[alldat$source == sr,]
-      for (i in seq(2,length(unique(temp$`6144col`)))) {
+      for (i in seq(1,length(unique(temp$`6144col`)))) {
         col <- unique(temp$`6144col`)[i]
         lf <- tail(temp$`6144col`[temp$`6144col` < col & temp$orf_name == 'BF_control' & !is.na(temp$orf_name)],1)
         rt <- temp$`6144col`[temp$`6144col` > col & temp$orf_name == 'BF_control' & !is.na(temp$orf_name)][1]
-        for (ii in seq(2,length(unique(temp$`6144row`[temp$`6144col` == col])))) {
+        for (ii in seq(1,length(unique(temp$`6144row`[temp$`6144col` == col])))) {
           row <- unique(temp$`6144row`[temp$`6144col` == col])[ii]
           up <- tail(temp$`6144row`[temp$`6144row` < row & temp$orf_name == 'BF_control' & !is.na(temp$orf_name)],1)
           dw <- temp$`6144row`[temp$`6144row` > row & temp$orf_name == 'BF_control' & !is.na(temp$orf_name)][1]
@@ -81,6 +84,8 @@ for (hr in hours$hours) {
       }
       diff_std <- sd(alldat$diff[alldat$source == sr],na.rm = T)
       diff_mean <- mean(alldat$diff[alldat$source == sr],na.rm = T)
+      # alldat$outlier[alldat$source == sr & !is.na(alldat$average) & alldat$diff > (diff_mean + 3*diff_std)] = 'Bigger'
+      # alldat$outlier[alldat$source == sr & !is.na(alldat$average) & alldat$diff < (diff_mean - 3*diff_std)] = 'Smaller'
       alldat$outlier[alldat$source == sr & !is.na(alldat$average) & alldat$diff > (diff_mean + 3*diff_std)] = 'Bigger'
       alldat$outlier[alldat$source == sr & !is.na(alldat$average) & alldat$diff < (diff_mean - 3*diff_std)] = 'Smaller'
       alldat$outlier[is.na(alldat$outlier)] = 'Normal'
@@ -88,7 +93,7 @@ for (hr in hours$hours) {
     }
     
     ggplot(alldat[alldat$average > 0,]) +
-      geom_point(aes(x = `6144col`, y = `6144row`, shape = colony, col = gap)) +
+      geom_point(aes(x = `6144col`, y = `6144row`, shape = colony, col = outlier)) +
       scale_x_continuous(breaks = seq(1,96,1),limits = c(1,96)) +
       scale_y_continuous(breaks = seq(1,64,1),limits = c(64,1),trans = 'reverse') +
       # scale_color_discrete(guide = F) +
@@ -98,6 +103,108 @@ for (hr in hours$hours) {
             axis.text = element_blank(),
             axis.ticks = element_blank())
     
+    
+    alldat$nearBig <- 'N'
+    for (o in alldat$pos) {
+      c = alldat$`6144col`[alldat$pos == o]
+      r = alldat$`6144row`[alldat$pos == o]
+      if (alldat$outlier[alldat$`6144col` == c & alldat$`6144row` == r] == 'Bigger') {
+        alldat$nearBig[alldat$`6144col` == c - 2 & alldat$`6144row` == r - 2 |
+                     alldat$`6144col` == c - 1 & alldat$`6144row` == r - 2 |
+                     alldat$`6144col` == c & alldat$`6144row` == r - 2 |
+                     alldat$`6144col` == c + 1 & alldat$`6144row` == r - 2 |
+                     alldat$`6144col` == c + 2 & alldat$`6144row` == r - 2 |
+                     alldat$`6144col` == c - 2 & alldat$`6144row` == r - 1 |
+                     alldat$`6144col` == c + 2 & alldat$`6144row` == r - 1 |
+                     alldat$`6144col` == c - 2 & alldat$`6144row` == r |
+                     alldat$`6144col` == c + 2 & alldat$`6144row` == r |
+                     alldat$`6144col` == c - 2 & alldat$`6144row` == r + 1 |
+                     alldat$`6144col` == c + 2 & alldat$`6144row` == r + 1 |
+                     alldat$`6144col` == c - 2 & alldat$`6144row` == r + 2 |
+                     alldat$`6144col` == c - 1 & alldat$`6144row` == r + 2 |
+                     alldat$`6144col` == c & alldat$`6144row` == r + 2 |
+                     alldat$`6144col` == c + 1 & alldat$`6144row` == r + 2 |
+                     alldat$`6144col` == c + 2 & alldat$`6144row` == r + 2] = 'B2'
+      }
+    }
+    
+    for (o in alldat$pos) {
+      c = alldat$`6144col`[alldat$pos == o]
+      r = alldat$`6144row`[alldat$pos == o]
+      if (alldat$outlier[alldat$`6144col` == c & alldat$`6144row` == r] == 'Bigger') {
+        alldat$nearBig[alldat$`6144col` == c - 1 & alldat$`6144row` == r - 1 |
+                     alldat$`6144col` == c & alldat$`6144row` == r - 1 |
+                     alldat$`6144col` == c + 1 & alldat$`6144row` == r - 1 |
+                     alldat$`6144col` == c - 1 & alldat$`6144row` == r |
+                     alldat$`6144col` == c + 1 & alldat$`6144row` == r |
+                     alldat$`6144col` == c - 1 & alldat$`6144row` == r + 1 |
+                     alldat$`6144col` == c & alldat$`6144row` == r + 1 |
+                     alldat$`6144col` == c + 1 & alldat$`6144row` == r + 1] = 'B1'
+      }
+    }
+    
+    alldat$nearSmall <- 'N'
+    for (o in alldat$pos) {
+      c = alldat$`6144col`[alldat$pos == o]
+      r = alldat$`6144row`[alldat$pos == o]
+      if (alldat$outlier[alldat$`6144col` == c & alldat$`6144row` == r] == 'Smaller') {
+        alldat$nearSmall[alldat$`6144col` == c - 2 & alldat$`6144row` == r - 2 |
+                         alldat$`6144col` == c - 1 & alldat$`6144row` == r - 2 |
+                         alldat$`6144col` == c & alldat$`6144row` == r - 2 |
+                         alldat$`6144col` == c + 1 & alldat$`6144row` == r - 2 |
+                         alldat$`6144col` == c + 2 & alldat$`6144row` == r - 2 |
+                         alldat$`6144col` == c - 2 & alldat$`6144row` == r - 1 |
+                         alldat$`6144col` == c + 2 & alldat$`6144row` == r - 1 |
+                         alldat$`6144col` == c - 2 & alldat$`6144row` == r |
+                         alldat$`6144col` == c + 2 & alldat$`6144row` == r |
+                         alldat$`6144col` == c - 2 & alldat$`6144row` == r + 1 |
+                         alldat$`6144col` == c + 2 & alldat$`6144row` == r + 1 |
+                         alldat$`6144col` == c - 2 & alldat$`6144row` == r + 2 |
+                         alldat$`6144col` == c - 1 & alldat$`6144row` == r + 2 |
+                         alldat$`6144col` == c & alldat$`6144row` == r + 2 |
+                         alldat$`6144col` == c + 1 & alldat$`6144row` == r + 2 |
+                         alldat$`6144col` == c + 2 & alldat$`6144row` == r + 2] = 'S2'
+      }
+    }
+    
+    for (o in alldat$pos) {
+      c = alldat$`6144col`[alldat$pos == o]
+      r = alldat$`6144row`[alldat$pos == o]
+      if (alldat$outlier[alldat$`6144col` == c & alldat$`6144row` == r] == 'Smaller') {
+        alldat$nearSmall[alldat$`6144col` == c - 1 & alldat$`6144row` == r - 1 |
+                         alldat$`6144col` == c & alldat$`6144row` == r - 1 |
+                         alldat$`6144col` == c + 1 & alldat$`6144row` == r - 1 |
+                         alldat$`6144col` == c - 1 & alldat$`6144row` == r |
+                         alldat$`6144col` == c + 1 & alldat$`6144row` == r |
+                         alldat$`6144col` == c - 1 & alldat$`6144row` == r + 1 |
+                         alldat$`6144col` == c & alldat$`6144row` == r + 1 |
+                         alldat$`6144col` == c + 1 & alldat$`6144row` == r + 1] = 'S1'
+      }
+    }
+
+    ggplot(alldat[alldat$average > 0 & alldat$orf_name == 'BF_control',]) +
+      # geom_point(aes(x = `6144col`, y = `6144row`, shape = colony, col = nearSmall)) +
+      geom_point(aes(x = `6144col`, y = `6144row`, shape = colony, col = nearBig)) +
+      scale_x_continuous(breaks = seq(1,96,1),limits = c(1,96)) +
+      scale_y_continuous(breaks = seq(1,64,1),limits = c(64,1),trans = 'reverse') +
+      # scale_color_discrete(guide = F) +
+      scale_shape_discrete(guide = F) +
+      theme_linedraw() +
+      theme(axis.title = element_blank(),
+            axis.text = element_blank(),
+            axis.ticks = element_blank())
+    
+    ggplot(alldat[alldat$orf_name == 'BF_control' & alldat$outlier != 'Bigger' & !is.na(alldat$average),]) +
+      geom_bar(aes(x = average, col = nearBig), stat = 'density') +
+      geom_bar(data = alldat[alldat$outlier == 'Bigger' & !is.na(alldat$average),],
+               aes(x = average, col = 'B'), stat = 'density') +
+      facet_wrap(.~source)
+    
+    ggplot(alldat[alldat$orf_name == 'BF_control' & alldat$outlier != 'Smaller' & !is.na(alldat$average),]) +
+      geom_bar(aes(x = average, col = nearSmall), stat = 'density') +
+      geom_bar(data = alldat[alldat$outlier == 'Smaller' & !is.na(alldat$average),],
+               aes(x = average, col = 'S'), stat = 'density') +
+      facet_wrap(.~source)
     
     jpegdat <- rbind(jpegdat, alldat)
     
