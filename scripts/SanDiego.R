779e057b8677fa0132b966c6c885134950a529a3
saur1n <dr.saurin.parikh@gmail.com>
2019-09-10 13:38:07 -0400

    SanDiego analysis init

---
+++ scripts/SanDiego.R
@@ -0,0 +1,11 @@
+##### SAN DIEGO ANALYSIS
+##### Author  : Saurin Parikh (dr.saurin.parikh@gmail.com)
+##### Date    : 09/10/2019
+
+##### INITIALIZE
+library(RMariaDB)
+library(ggplot2)
+library(ggExtra)
+library(gridExtra)
+library(ggpubr)
+source("R/functions/initialize.sql.R")
